9aab6ff3c8ec51ac8084c028d8ca78c9ce3f0332
pandasweta <130973588+pandasweta@users.noreply.github.com>
2024-02-08 13:07:19 +0100

    In the index.htm added the axios and added instructions on index.js
    We added the axios link in the html file. Also added the API and worked around the API in index.js to generate quotes.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-function quoteGenerator(event) {
-    event.preventDefault();
-    // let quotesElement = document.querySelector("h2")
-    // quotesElement.innerHTML = "Love recognizes no barriers.It jumps hurdles, leaps fences, penetrates walls to arrive at its destination full of hope. Maya Angelou"
+function displayQuote(response) {
 
     new Typewriter("h2", {
-        strings: "Love recognizes no barriers.It jumps hurdles, leaps fences, penetrates walls to arrive at its destination full of hope. Maya Angelou",
+        strings: `${response.data.answer}`,
         autoStart: true,
         cursor: null,
         delay: 5,
 
     });
+    console.log(response.data);
+
+
+}
+
+
+function quoteGenerator(event) {
+    event.preventDefault();
+    let apiKey = "at7ddf3o73b01eae74140de7cdd2c6b9";
+    let topicElement = document.querySelector("#topic");
+    let prompt = `User instrustions: Generate a nice single-line quote on ${topicElement.value}`;
+    let context = "You are a wise and helpful assistant who love to share knwoledge. Please share your knowledge based on User instrustions in each line.";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    // console.log(topicElement.value);
+    console.log('Generating quotes');
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Prompt: ${context}`);
+
+
+
+    axios.get(apiURL).then(displayQuote)
+
+
 
 }
 
