2466ab1911ae56091db51399fcdf8520fce7f86c
pandasweta <130973588+pandasweta@users.noreply.github.com>
2024-02-08 12:19:17 +0100

    Added the basic javascript to index.html
    Added the form-handling, typewrite.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// alert("Hello");
+function quoteGenerator(event) {
+    event.preventDefault();
+    // let quotesElement = document.querySelector("h2")
+    // quotesElement.innerHTML = "Love recognizes no barriers.It jumps hurdles, leaps fences, penetrates walls to arrive at its destination full of hope. Maya Angelou"
+
+    new Typewriter("h2", {
+        strings: "Love recognizes no barriers.It jumps hurdles, leaps fences, penetrates walls to arrive at its destination full of hope. Maya Angelou",
+        autoStart: true,
+        cursor: null,
+        delay: 5,
+
+    });
+
+}
+
+
+
+let quoteForm = document.querySelector("#quotes-form-generator");
+quoteForm.addEventListener("submit", quoteGenerator);
