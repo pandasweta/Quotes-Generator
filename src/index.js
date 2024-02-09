4505f0117de6ba61d3bafefea6b7a24ab1e92e0a
pandasweta <130973588+pandasweta@users.noreply.github.com>
2024-02-09 12:00:11 +0100

    Improvised the user interface and added blinking effect.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 function displayQuote(response) {
 
-    new Typewriter("h2", {
+
+
+    new Typewriter("#quote", {
         strings: `${response.data.answer}`,
         autoStart: true,
         cursor: null,
-        delay: 5,
+        delay: 1,
 
     });
     console.log(response.data);
@@ -21,19 +23,22 @@ function quoteGenerator(event) {
     let context = "You are a wise and helpful assistant who love to share knwoledge. Please share your knowledge based on User instrustions in each line.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     // console.log(topicElement.value);
+    let poemElement = document.querySelector("h2");
+    poemElement.classList.remove("hidden")
+    poemElement.innerHTML = `<h2 class="blink">Generating a nice quote for you about ${topicElement.value}...</h2>`;
     console.log('Generating quotes');
     console.log(`Prompt: ${prompt}`);
     console.log(`Prompt: ${context}`);
-
-
-
     axios.get(apiURL).then(displayQuote)
-
-
 
 }
 
 
-
 let quoteForm = document.querySelector("#quotes-form-generator");
 quoteForm.addEventListener("submit", quoteGenerator);
+
+
+
+
+
+
